678e339d98dc975ffa4b986c0e7d859e508be196
griverorz <griverorz@gmail.com>
2026-07-11 19:55:35 +0000

    Deploying to gh-pages from @ griverorz/griverorz.github.io@72c65a5730629a25d0312e52f0a721fb0f901584 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,12 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "books-the-godfather",
-          title: 'The Godfather',
+  },{id: "nav-research",
+          title: "Research",
           description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-retooling-politics-is-now-available-in-cambridge-core",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
+        },{id: "news-retooling-politics-is-now-available-in-cambridge-core",
           title: 'Retooling Politics is now available in Cambridge Core.',
           description: "",
           section: "News",},{id: "news-our-article-on-the-prediction-of-corruption-in-procurement-is-now-out-at-the-ijf",
@@ -53,52 +55,7 @@ ninja.data = [{
           section: "News",},{id: "news-our-working-paper-about-polarization-and-trust-has-come-out",
           title: 'Our working paper about polarization and trust has come out',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{
+          section: "News",},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
